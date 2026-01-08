1e72267cd96185a74f472a171ebb61012952dda9
two44two <72901294+two44two@users.noreply.github.com>
2026-01-08 10:39:38 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,34 @@
-const CACHE_NAME = "solitaire-v1";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./solitaire_compiled.js",
-  "./ribbon_no_text.png",
-  "./mobile_portrait.css",
-  "./ipad.css",
-  "./default.css",
-  "./fonts.css",
-  "./card-sprite.png",
-  "./Material-Icons-Extended.woff2"
+const CACHE_NAME = 'solitaire-pwa-v1';
+const urlsToCache = [
+    'standalone.html',
+    'default.css',
+    'ipad.css',
+    'mobile_portrait.css',
+    'solitaire_compiled.js',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png',
+    'material-icons-extended.woff2'
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
+    );
 });
